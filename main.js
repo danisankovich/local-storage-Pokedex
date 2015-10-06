922b7ad161760d227485008f5d9c16d740ea4267
Michael Sankovich <misankovich@gmail.com>
2015-10-05 21:22:01 -0700

    party size restrictions. Conditional additions. Move to party buttons.

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
 var app = angular.module('myApp', []);
 
 app.controller('mainCtrl', ['$scope', '$http', "$location", function($scope, $http, $location) {
+	$scope.held = localStorage.getItem('myParty');
+	$scope.myParty = (localStorage.getItem('myParty')!==null) ? JSON.parse($scope.held) : [];
+	localStorage.setItem('myParty', JSON.stringify($scope.myParty));
+	$scope.partyPokemon = $scope.myParty;
+
+
 	$scope.saved = localStorage.getItem('myPokemon');
 	$scope.myPokemon = (localStorage.getItem('myPokemon')!==null) ? JSON.parse($scope.saved) : [];
 	localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
-	$scope.partyPokemon = $scope.myPokemon.slice(0, 6);
-	$scope.pcPokemon = $scope.myPokemon.slice(6, $scope.myPokemon.length);
-	if ($scope.pcPokemon.length > 0) {
-		$scope.hasSixPokemon = true;
-	}
-	else {
-		$scope.hasSixPokemon = false;
-	}
+	$scope.pcPokemon = $scope.myPokemon;
+
+	var checkParty = function() {
+		if ($scope.partyPokemon.length === 6) {
+			$scope.hasSixPokemon = true;
+		}
+		else {
+			$scope.hasSixPokemon = false;
+		}
+	};
 	$scope.addPokemon = function() {
 			$http.get('http://pokeapi.co/api/v1/pokedex/1').success(function(response) {
 				angular.forEach(response.pokemon, function(poke) {
 					if(poke.name===$scope.pokemonText){
-						console.log(poke);
-						$scope.myPokemon.push({
-							pokemon: poke.name,
-							nickname: $scope.nicknameText,
-							imageUrl: 'http://img.pokemondb.net/artwork/'+$scope.pokemonText+'.jpg'
-						});
-						console.log($scope.myPokemon);
-						localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
+						checkParty();
+						if($scope.hasSixPokemon) {
+							$scope.myPokemon.push({
+								pokemon: poke.name,
+								nickname: $scope.nicknameText,
+								imageUrl: 'http://img.pokemondb.net/artwork/'+$scope.pokemonText+'.jpg'
+							});
+							localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
+						}
+						else {
+							$scope.myParty.push({
+								pokemon: poke.name,
+								nickname: $scope.nicknameText,
+								imageUrl: 'http://img.pokemondb.net/artwork/'+$scope.pokemonText+'.jpg'
+							});
+							localStorage.setItem('myParty', JSON.stringify($scope.myParty));
+						}
 					}
 				});
 				window.location.reload();
@@ -34,21 +51,24 @@ app.controller('mainCtrl', ['$scope', '$http', "$location", function($scope, $ht
 			});
 		};
 
-  $scope.releasePokemon = function() {
-    console.log($scope.myPokemon);
+  $scope.releasePokemonFromPC = function() {
     $scope.myPokemon.splice(this.$index, 1);
-    console.log(this.$index);
     localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
+		window.location.reload();
+  };
+  $scope.releasePokemonFromParty = function() {
+    $scope.myParty.splice(this.$index, 1);
+    localStorage.setItem('myParty', JSON.stringify($scope.myParty));
 		window.location.reload();
   };
 
   $scope.moveToPC = function() {
 		console.log(this.pokemon);
 		tempPoke = this.pokemon;
-    $scope.myPokemon.splice(this.$index, 1);
+    $scope.myParty.splice(this.$index, 1);
 		$scope.myPokemon.push(tempPoke);
-    console.log($scope.myPokemon);
     localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
+    localStorage.setItem('myParty', JSON.stringify($scope.myParty));
 		window.location.reload();
   };
 
@@ -56,5 +76,19 @@ app.controller('mainCtrl', ['$scope', '$http', "$location", function($scope, $ht
 		$scope.myPokemon = [];
 		localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
 		window.location.reload();
+	};
+
+	$scope.putInParty = function() {
+		checkParty();
+		if($scope.hasSixPokemon === false) {
+			tempPoke = this.pokemon;
+			console.log(tempPoke);
+			if ($scope.hasSixPokemon === false) {
+				$scope.myParty.push(tempPoke);
+				$scope.myPokemon.splice(this.$index, 1);
+				localStorage.setItem('myParty', JSON.stringify($scope.myParty));
+				localStorage.setItem('myPokemon', JSON.stringify($scope.myPokemon));
+			}
+		}
 	};
 }]);
